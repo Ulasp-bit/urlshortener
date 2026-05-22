ecf6a2c7789e2659b2c29bbdc04a92af42fe302d
Ulasp-bit <ulaspbit@gmail.com>
2026-05-22 14:36:05 -0600

    Acortador de URLs en .NET 10

---
+++ Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// diccionario para guardar
+var memoryDB = new Dictionary<string, string>();
+
+// endpoint para acortar la url
+app.MapPost("/shorten", (CreateUrlRequest petition) =>
+{
+   if (string.IsNullOrEmpty(petition.LongUrl))
+    {
+        return Results.BadRequest("La URL no puede estar vacia.");
+    } 
+
+    // generar un codigo aleatorio de 6 caracteres
+    string shortCode = Guid.NewGuid().ToString().Substring(0,6);
+
+    // guardarlo en el diccionario
+    memoryDB[shortCode] = petition.LongUrl;
+
+    // devolver el codigo al usuario
+    return Results.Ok(new { Code = shortCode, shortUrl = $"https://localhost:5067/{shortCode}"});
+});
+
+app.MapGet("/{code}", (string code) =>
+{
+    // buscar el codigo en el diccionario
+   if (memoryDB.TryGetValue(code, out var longUrl))
+    {
+        // si existe, redirigimos al usuario a la web real
+        return Results.Redirect(longUrl);
+    } 
+
+    // si no existe, devolvemos un error 404
+    return Results.NotFound("El codigo no existe") ;
+});
+
+app.MapGet("/", () => "Hola");
+
+app.Run();
+
+public class CreateUrlRequest
+{
+    public string LongUrl { get; set; } = string.Empty;
+}
